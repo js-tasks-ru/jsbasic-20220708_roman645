82da87d609d84d576c34c0be204cd0605ee9e0a3
Don-Pedre <103751215+Olgao89@users.noreply.github.com>
2022-07-18 15:30:45 +0300

    checkspam_truncate

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase(str);
+  let result;
+    if (str.includes('1xbet') || str.includes('xxx')) {
+      return true;
+    } else {
+    return false;
+  }
+  return result;
 }
