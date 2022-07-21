884e723b498c16cf86ef91ac66f98ff0700e8fd4
Don-Pedre <103751215+Olgao89@users.noreply.github.com>
2022-07-21 16:11:45 +0300

    sumsalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,7 +3,7 @@ function sumSalary(salaries) {
   let sum = 0;
 
   for (let key in salaries) {
-    if (typeof salaries[key] == 'number') {
+    if (typeof(salaries[key]) === 'number' && isFinite(salaries[key])) {
     sum += salaries[key];
     };
   };
