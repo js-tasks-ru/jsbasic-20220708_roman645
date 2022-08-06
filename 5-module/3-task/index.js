76e8914098634b9c83b07aba8cd6c3b4e57de988
Don-Pedre <103751215+Olgao89@users.noreply.github.com>
2022-08-07 00:36:02 +0300

    initcarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,19 @@
 function initCarousel() {
-  // ваш код...
-}
+  let elem = document.querySelector('.carousel__inner');
+  let width = elem.offsetWidth;
+  let position = 0;
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+
+    arrowLeft.onclick = function() {
+    position += width;
+    elem.style.transform = `translateX(${position}px)`;
+  };
+
+  arrowRight.onclick = function() {
+    position -= width;
+    elem.style.transform = `translateX(${position}px)`;
+  };
+};
+
+initCarousel();
