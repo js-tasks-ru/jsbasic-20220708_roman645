b5bce2bc093990437f43d77dedea1d9fc0d3a109
Don-Pedre <103751215+Olgao89@users.noreply.github.com>
2022-08-12 23:20:03 +0300

    productcard

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,45 @@
 export default class ProductCard {
-  constructor(product) {
+  
+  constructor(productCard) {
+   
+    
+
+
+    
+    html(); {
+      let template = createElement(`  
+      <div class="card">
+      ${this.product.map(obj =>
+        `
+    <div class="card__top">
+        <img src="/assets/images/products/${obj.image}" class="card__image" alt="product">
+        <span class="card__price">€${obj.price}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${obj.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+    `).join('')}
+</div> `)
+this.elem.innerHTML = template; 
+    }
+    deleteRow(); {
+      for (const button of this.elem.querySelectorAll("button"))
+        button.addEventListener('click', (event) =>
+        event.target.closest("tr").remove())
+    }
   }
 }
+
+let product = {
+  name: "Laab kai chicken salad", // название товара
+  price: 10, // цена товара
+  category: "salads", // категория, к которой он относится, нам это понадобится чуть позже
+  image: "laab_kai_chicken_salad.png", // название картинки товара
+  id: "laab-kai-chicken-salad" // уникальный идентификатор товара, нужен для добавления товара в корзину
+}
+
+let productCard = new ProductCard(product);
+
